33753b71d08f49d4e8ff70a5a66b69fdce0daa55
KRISHNA K <kkrishna22572@gmail.com>
2024-03-07 14:24:05 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl text-orange-600">
+        How to Install Shadcn UI in svelteKit
+    </h1>
+    <Button>Button</Button>
+</div>
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
