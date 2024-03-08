a7db030a43c6376ea3e96ef5da6da410fdb54b08
KRISHNA K <kkrishna22572@gmail.com>
2024-03-09 00:20:52 +0530

    [feat] : complete ui v_1.0.0 :white_check_mark:

---
+++ src/lib/components/nav/nav.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import LogoIcon from "$lib/assets/LogoIcon.svg";
+  import LogoText from "$lib/assets/LogoText.svg";
+  import ProfileImage from "$lib/assets/Profile2.png";
+  import ChevronDown from "$lib/assets/ChevronDown.svg";
+  import PlusIcon from "$lib/assets/PlusIcon.svg";
+  import HomeIcon from "$lib/assets/HomeIcon.svg";
+  import StoryIcon from "$lib/assets/StoryIcon.svg";
+  import BookIcon from "$lib/assets/BookIcon.svg";
+</script>
+
+<div
+  class="nav max-w-screen-xl w-full px-2 xl:px-0 flex justify-between mx-auto border-dotted border-b-2"
+>
+  <div class="flex items-center gap-1">
+    <img src={LogoIcon} alt="duggup logo icon" class="w-7 h-7" />
+    <img src={LogoText} alt="duggup" class="hidden md:block" />
+  </div>
+  <div class="nav-right flex gap-5 md:gap-7 lg:gap-10 items-center">
+    <div class="flex border-black gap-5 md:gap-7 lg:gap-10 items-center">
+      <div class="flex gap-5 md:gap-7 lg:gap-10 border-black">
+        <div class="flex items-center border-black">
+          <div class="p-1 border-black flex items-center justify-center">
+            <img src={HomeIcon} alt="Home Icon" class="w-6" />
+          </div>
+          <p class="p-2 hidden sm:block">Home</p>
+        </div>
+        <div class="flex items-center">
+          <div class="p-1 flex items-center justify-center">
+            <img src={StoryIcon} alt="Home Icon" class="w-6" />
+          </div>
+          <div class="p-2 hidden sm:block">Explore</div>
+        </div>
+        <div class="flex items-center">
+          <div class="p-1 flex items-center justify-center">
+            <img src={BookIcon} alt="Home Icon" class="w-6" />
+          </div>
+          <div class="p-2 hidden sm:block">Learn</div>
+        </div>
+      </div>
+      <div
+        class=" max-h-9 h-fit border-[0.5px] border-[#4D4D4D] shadow-[0_4px_0_0_#4D4D4D] rounded px-1 sm:px-2 md:px-3 lg:px-4 bg-white flex items-center"
+      >
+        <div class="p-1 flex items-center justify-center">
+          <img src={PlusIcon} alt="Plus Icon" class="w-4 h-4" />
+        </div>
+        <div class="w-[46px] p-2">Post</div>
+      </div>
+    </div>
+    <div class="profile pr-2 lg:px-3 py-3 gap-1 flex items-center">
+      <div
+        class="rounded-full h-12 w-12 border-[0.5px] border-[#4D4D4D] overflow-clip"
+      >
+        <img
+          src={ProfileImage}
+          alt="user profile"
+          class="object-cover scale-105"
+        />
+      </div>
+      <div class="hidden md:block line-clamp-1 overflow-ellipsis text-nowrap">
+        Krishna Kiran
+      </div>
+      <div class="hidden md:flex w-4 h-4 items-center justify-center">
+        <img src={ChevronDown} alt="chevron-down icon" class="object-contain" />
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/lib/components/profile/profile.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import Profile2 from "$lib/assets/Profile2.png";
+  import Logo2Icon from "$lib/assets/Logo2Icon.svg";
+  import LinkIcon from "$lib/assets/LinkIcon.svg";
+  import ThreeDotsIcon from "$lib/assets/ThreeDotsIcon.svg";
+</script>
+
+<div class="flex mx-auto p-8 max-w-screen-lg gap-8 md:gap-12">
+  <div class="flex flex-col items-center">
+    <div class="flex items-center justify-center rounded-full overflow-clip">
+      <img src={Profile2} alt="User Profile" class="cursor-pointer" />
+    </div>
+    <p class="font-bold text-[24px] text-lg text-center text-[#4B5C6D] underline-offset-1 cursor-pointer hover:underline decoration-2 decoration-blue-500">
+      Krishna <br /> Kiran
+    </p>
+  </div>
+  <div class="flex-1 flex flex-col py-4 gap-5">
+    <div class="flex justify-between">
+      <p class="w-3/4 font-normal text-[14px] text-base text-[#141618]">
+        Co-Founder and CEO at Duggup - Social network for people in tech.
+        Ex-Amazon Head of Engineering. I write hot-takes on building a business,
+        shipping delightful products and accelerating product and career growth.
+      </p>
+      <div class="  flex flex-col gap-2 items-end">
+        <img
+          src={Logo2Icon}
+          alt="Duggup icon"
+          class="w-9 h-9 rounded-full overflow-hidden cursor-pointer"
+        />
+
+        <div class="text-right flex flex-col gap-1">
+          <p class="font-bold text-black text-[1rem] leading-5 cursor-pointer hover:underline underline-offset-1 decoration-sky-500">Diggup</p>
+          <p class="text-right text-xs leading-3 text-[#7A9299]">
+            Co-Founder and CEO
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <div class=" flex items-center">
+        <button
+          class="p-2 max-h-9 rounded px-4 bg-white hover:bg-[#0066FF] hover:text-white border-[0.5px] border-[#4D4D4D] hover:border-[#0066FF] shadow-[0_4px_0_0_#4D4D4D] hover:shadow-[0_4px_0_0_#003A91]"
+          >Followed</button
+        >
+      </div>
+      <div class="flex items-center gap-4 pl-4">
+        <div class="flex items-center gap-1 text-[#4D4D4D] cursor-pointer">
+          <span class="text-sm leading-5 text-[#4D4D4D]">My website</span>
+          <img src={LinkIcon} alt="Link icon" class="w-3 h-3" />
+        </div>
+        <img
+          src={ThreeDotsIcon}
+          alt="ThreeDots Icon"
+          class="text-[#A5B9D3] w-7 h-7 cursor-pointer"
+        />
+      </div>
+    </div>
+  </div>
+</div>
